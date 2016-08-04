229fbfade28bce785cd8bc3bf4d443002ba1815f
yaroslav-shcherbyniuk <yaroslav_syv@mail.ru>
2016-08-05 01:44:15 +0300

    Change project types

---
+++ test/TestClassLibrary1/Class1Tests.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestClassLibrary1
+{
+    public class Class1Tests
+    {
+        public Class1Tests()
+        {
+        }
+
+
+        [Fact]
+        public void Add()
+        {
+            Assert.Equal(4, Class1.Add(2, 2));
+        }
+    }
+}
